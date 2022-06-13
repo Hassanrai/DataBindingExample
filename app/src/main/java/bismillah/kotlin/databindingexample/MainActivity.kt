f58e1869905cd327e8eb3bebb016a7f3688cfea2
Hassanrai <hassanmehmood401@gmail.com>
2022-06-13 19:30:43 +0200

    Initial commit
    Data Binding on LiveData object.
    1) one way data binding through TextView
    2) Two way data binding through EditText

--- app/src/main/java/bismillah/kotlin/databindingexample/MainActivity.kt
+++ app/src/main/java/bismillah/kotlin/databindingexample/MainActivity.kt
@@ -3,16 +3,21 @@ package bismillah.kotlin.databindingexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import bismillah.kotlin.databindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val quoteObj = Quote("Love is Beautiful.", "Hassan")
-        binding.quote = quoteObj
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
     }
 }
